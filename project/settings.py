eec29f7033ab3f74d229ca37e7b26623ae2e4808
sardorabdullayev0099@gmail.com <sardorabdullayev0099@gmail.com>
2025-12-23 22:46:10 +0500

    changed port

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 GLOBAL_WORKER_PIN = os.getenv("GLOBAL_WORKER_PIN")
 
 # CSRF xavfsizligi uchun ishonchli domenlar ro'yxati
