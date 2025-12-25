264ca7f5dd464c9a812f08476f8d776eadd19f2f
Sardor <sardorabdullayev0099@gmail.com>
2025-12-25 16:14:05 +0500

    chenged root

--- project/settings.py
+++ project/settings.py
@@ -160,7 +160,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # media/ papkasi loyiha ildizida yaratiladi
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # O'z loyihangiz manzilini yozing
