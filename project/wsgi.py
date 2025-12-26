668115e92ac28123609525ac8162916ca56640de
Sardor <sardorabdullayev0099@gmail.com>
2025-12-26 15:11:01 +0500

    urls va wsgi.py alishdi

--- project/wsgi.py
+++ project/wsgi.py
@@ -7,15 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-import os
+ 
 
+import os
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling, MediaCling  
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-
-application = get_wsgi_application()
-
-
-
-
-
+ 
+application = Cling(MediaCling(get_wsgi_application()))
