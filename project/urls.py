fecb9076b78166d06de459e6532bb8db884f83b5
Sardor <sardorabdullayev0099@gmail.com>
2026-01-03 17:21:23 +0500

    Comment out URL patterns for media and static files
    Commented out production and development URL patterns for media and static files.

--- project/urls.py
+++ project/urls.py
@@ -9,14 +9,15 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("checklist.urls")),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# Productionda (DEBUG=False) rasmlar ko'rinishi uchun bu qism kerak
-if not settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-    ]
-else:
-    # Development rejimida (DEBUG=True)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# # Productionda (DEBUG=False) rasmlar ko'rinishi uchun bu qism kerak
+# if not settings.DEBUG:
+#     urlpatterns += [
+#         re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+#         re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+#     ]
+# else:
+#     # Development rejimida (DEBUG=True)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
