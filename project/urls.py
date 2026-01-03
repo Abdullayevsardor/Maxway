3513177c2008bec6859fc5920211686ed88bdb45
Sardor <sardorabdullayev0099@gmail.com>
2026-01-03 16:33:52 +0500

    Restore Django URL patterns and add re_path
    Uncommented the Django URL configuration and added re_path.

--- project/urls.py
+++ project/urls.py
@@ -1,19 +1,4 @@
  
-# from django.contrib import admin
-# from django.urls import path, include
-# from django.conf import settings
-# from django.conf.urls.static import static
-
-# urlpatterns = [
-#     path("admin/", admin.site.urls),
-#     path("", include("checklist.urls")),
-# ]
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
 from django.contrib import admin
 from django.urls import path, include, re_path # re_path qo'shildi
 from django.conf import settings
